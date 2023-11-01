6b74b53cff77e9247b763773986133044db5a03d
Kherilirli <146070688+Kherilirli@users.noreply.github.com>
2023-11-01 10:46:12 +0700

    Delete DDP directory

--- DDP/pekan3/konversiSuhu.py
+++
@@ -1,5 +0,0 @@
-celcius = 30
-
-f = int(celcius * 9/5) + 32
-
-print("hasil konversi kefahrenheit", f)
--- DDP/pekan5/list.py
+++
@@ -1,6 +0,0 @@
-kendaraan = ["beat", "sepeda motor", 400, "hitam", 2]
-
-kendaraan.append('30jt')
-kendaraan.append("matic")
-kendaraan.insert(2,"honda")
-print(kendaraan)
